68e9e694a37ecca5ef065f7f223d9c5610c61efb
mahmudul-hasan-870 <mahmudulhasan7780@gmail.com>
2023-01-14 10:04:45 +0600

    first commit

---
+++ lib/evants.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class EvantsdPage extends StatefulWidget {
+  const EvantsdPage({Key? key}) : super(key: key);
+
+  @override
+  State<EvantsdPage> createState() => _EvantsdPageState();
+}
+
+class _EvantsdPageState extends State<EvantsdPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text("EvantsdPage"),
+      ),
+    );
+  }
+}
